0290727c543f0296721b94f02d8eecccf10c8b2b
Michael Bucher <michael@axlabs.com>
2022-02-21 13:41:51 +0100

    Add initial NewContainerPutRequest method

--- go.mod
+++ go.mod
@@ -1,21 +1,10 @@
 module github.com/AxLabs/neofs-api-shared-lib
 
-go 1.17
+go 1.16
 
 require (
-	github.com/nspcc-dev/neofs-api-go v1.30.0
+	github.com/nspcc-dev/neofs-api-go/v2 v2.11.2-0.20220127135316-32dd0bb3f9c5
 	github.com/nspcc-dev/neofs-crypto v0.3.0
+	github.com/nspcc-dev/neofs-sdk-go v1.0.0-rc.1
 	google.golang.org/protobuf v1.27.1
-)
-
-require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/mr-tron/base58 v1.1.2 // indirect
-	github.com/nspcc-dev/rfc6979 v0.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.0.0-20210928044308-7d9f5e0b762b // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20210928142010-c7af6a1a74c9 // indirect
-	google.golang.org/grpc v1.41.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"crypto/elliptic"
 	"encoding/hex"
 	"fmt"
-	"github.com/nspcc-dev/neofs-api-go/rpc/message"
-	"github.com/nspcc-dev/neofs-api-go/v2/container"
-	signpkg "github.com/nspcc-dev/neofs-api-go/v2/signature"
+	v2container "github.com/nspcc-dev/neofs-api-go/v2/container"
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
+	"github.com/nspcc-dev/neofs-api-go/v2/rpc/message"
+	v2session "github.com/nspcc-dev/neofs-api-go/v2/session"
+	v2signature "github.com/nspcc-dev/neofs-api-go/v2/signature"
 	crypto "github.com/nspcc-dev/neofs-crypto"
+	"github.com/nspcc-dev/neofs-sdk-go/container"
+	sigutil "github.com/nspcc-dev/neofs-sdk-go/util/signature"
+	"github.com/nspcc-dev/neofs-sdk-go/version"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"math/big"
@@ -33,7 +38,7 @@ func main() {
 
 	jsonFromJava := "{\n  \"body\": {\n    \"container\": {\n      \"version\": {\n        \"major\": 2,\n        \"minor\": 11\n      },\n      \"ownerID\": {\n        \"value\": \"A9+X+2Xt74Dy/JmsSuTv0aMMUZwH+LNiF4J4fyiBqbe1\"\n      },\n      \"nonce\": \"L4Mz1w==\",\n      \"basicACL\": 532660223,\n      \"attributes\": [{\n        \"key\": \"key\",\n        \"value\": \"val\"\n      }],\n      \"placementPolicy\": {\n        \"replicas\": [{\n          \"count\": 2\n        }],\n        \"containerBackupFactor\": 1\n      }\n    }\n  },\n  \"metaHeader\": {\n    \"version\": {\n      \"major\": 2,\n      \"minor\": 11\n    },\n    \"epoch\": \"10\",\n    \"ttl\": 1000\n  }\n}"
 
-	pr := &container.PutRequest{}
+	pr := &v2container.PutRequest{}
 
 	m := pr.ToGRPCMessage().(proto.Message)
 	err = protojson.Unmarshal([]byte(jsonFromJava), m)
@@ -44,7 +49,7 @@ func main() {
 
 	_ = pr.FromGRPCMessage(m)
 
-	err = signpkg.SignServiceMessage(priv, pr)
+	err = v2signature.SignServiceMessage(priv, pr)
 	if err != nil {
 		fmt.Errorf(err.Error())
 	}
@@ -60,6 +65,7 @@ func main() {
 
 //export SignServiceMessage
 func SignServiceMessage(key *C.char, json *C.char) *C.char {
+	getPrivKey(key)
 	keyStr := C.GoString(key)
 	jsonStr := C.GoString(json)
 
@@ -81,7 +87,7 @@ func SignServiceMessage(key *C.char, json *C.char) *C.char {
 	wif, err := crypto.WIFEncode(priv)
 	fmt.Printf("WIF: %s\n", wif)
 
-	pr := &container.PutRequest{}
+	pr := &v2container.PutRequest{}
 
 	m := pr.ToGRPCMessage().(proto.Message)
 	err = protojson.Unmarshal([]byte(jsonStr), m)
@@ -94,7 +100,7 @@ func SignServiceMessage(key *C.char, json *C.char) *C.char {
 		panic(err)
 	}
 
-	err = signpkg.SignServiceMessage(priv, pr)
+	err = v2signature.SignServiceMessage(priv, pr)
 	if err != nil {
 		panic(err)
 	}
@@ -111,7 +117,129 @@ func SignServiceMessage(key *C.char, json *C.char) *C.char {
 	return cstr
 }
 
+func getPrivKey(key *C.char) *ecdsa.PrivateKey {
+	keyStr := C.GoString(key)
+	bytes, err := hex.DecodeString(keyStr)
+	die(err)
+	k := new(big.Int)
+	k.SetBytes(bytes)
+	priv := new(ecdsa.PrivateKey)
+	curve := elliptic.P256()
+	priv.PublicKey.Curve = curve
+	priv.D = k
+	priv.PublicKey.X, priv.PublicKey.Y = curve.ScalarBaseMult(k.Bytes())
+	return priv
+}
+
 //export VerifyServiceMessage
 func VerifyServiceMessage(msg interface{}) error {
-	return signpkg.VerifyServiceMessage(msg)
+	return v2signature.VerifyServiceMessage(msg)
+}
+
+type (
+	//CallOption func(*callOptions)
+
+	callOptions struct {
+		version *version.Version
+		//xHeaders []*session.XHeader
+		ttl uint32
+		//epoch    uint64
+		//key      *ecdsa.PrivateKey
+		//session  *session.Token
+		//bearer   *token.BearerToken
+	}
+)
+
+func defaultCallOptions() *callOptions {
+	return &callOptions{
+		version: version.Current(),
+		ttl:     2,
+	}
+}
+
+func v2MetaHeaderFromOpts(options *callOptions) *v2session.RequestMetaHeader {
+	meta := new(v2session.RequestMetaHeader)
+	meta.SetVersion(options.version.ToV2())
+	meta.SetTTL(options.ttl)
+	//meta.SetEpoch(options.epoch)
+
+	//xhdrs := make([]*v2session.XHeader, len(options.xHeaders))
+	//for i := range options.xHeaders {
+	//	xhdrs[i] = options.xHeaders[i].ToV2()
+	//}
+
+	//meta.SetXHeaders(xhdrs)
+
+	//if options.bearer != nil {
+	//	meta.SetBearerToken(options.bearer.ToV2())
+	//}
+
+	//meta.SetSessionToken(options.session.ToV2())
+
+	return meta
+}
+
+func die(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getContainerFromV2(v2Container *C.char) (*container.Container, error) {
+	sdkContainer := new(container.Container)
+	containerBytes, err := hex.DecodeString(C.GoString(v2Container))
+	if err != nil {
+		return nil, err
+	}
+	err = sdkContainer.Unmarshal(containerBytes)
+	if err != nil {
+		return nil, err
+	}
+	return sdkContainer, nil
+}
+
+func NewContainerPutRequest(key *C.char, v2Container *C.char) (*C.char, error) {
+	privKey := getPrivKey(key)
+
+	cnr, err := getContainerFromV2(v2Container)
+	if err != nil {
+		return nil, err
+	}
+
+	// form request body
+	reqBody := new(v2container.PutRequestBody)
+	reqBody.SetContainer(cnr.ToV2())
+
+	// sign cnr
+	signWrapper := v2signature.StableMarshalerWrapper{SM: reqBody.GetContainer()}
+	err = sigutil.SignDataWithHandler(privKey, signWrapper, func(key []byte, sig []byte) {
+		containerSignature := new(refs.Signature)
+		containerSignature.SetKey(key)
+		containerSignature.SetSign(sig)
+		reqBody.SetSignature(containerSignature)
+	}, sigutil.SignWithRFC6979())
+	if err != nil {
+		return nil, err
+	}
+
+	// form meta header
+	var meta v2session.RequestMetaHeader
+	meta.SetSessionToken(cnr.SessionToken().ToV2())
+
+	// form request
+	var req v2container.PutRequest
+	req.SetBody(reqBody)
+	req.SetMetaHeader(&meta)
+	return getMessageCChar(&req)
+}
+
+func getMessageCChar(req message.Message) (*C.char, error) {
+	jsonAfter, err := message.MarshalJSON(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(jsonAfter))
+
+	return C.CString(string(jsonAfter)), nil
 }
