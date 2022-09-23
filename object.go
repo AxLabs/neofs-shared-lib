c498151980f437e38fffcba9468985a2e1b1d847
Michael Bucher <michael@axlabs.com>
2022-09-23 16:27:20 +0200

    Add initial basic code for ObjectPut

--- object.go
+++ object.go
@@ -9,17 +9,188 @@ package main
 #endif
 */
 import "C"
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	neofsclient "github.com/nspcc-dev/neofs-sdk-go/client"
+	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
+	"reflect"
+	"sync"
+	"unsafe"
+)
 
 /*
 ----Object----
-Get
 Put
+Get
 Delete
 Head
 Search
 GetRange
 GetRangeHash
 */
+
+//export ObjectPutInit
+func ObjectPutInit(clientID *C.char) C.pointerResponse {
+	ctx := context.Background()
+
+	var prmObjectPutInit neofsclient.PrmObjectPutInit
+
+	neofsClient, err := getClient(clientID)
+	if err != nil {
+		return pointerResponseClientError()
+	}
+	neofsClient.mu.Lock()
+
+	// 	prmObjectPutInit.SetCopiesNumber(uint32)
+
+	w, err := neofsClient.client.ObjectPutInit(ctx, prmObjectPutInit)
+	neofsClient.mu.Unlock()
+	if err != nil {
+		return pointerResponseError(err.Error())
+	}
+
+	u, err := uuid.NewUUID()
+	if err != nil {
+		return pointerResponseError(err.Error())
+	}
+
+	//w.MarkLocal() // optional
+	//w.UseKey() // optional
+
+	if wMap == nil {
+		initWriterMap(u, w)
+	} else {
+		wMap.put(u, w)
+	}
+	return pointerResponse(reflect.TypeOf(u), []byte(u.String()))
+}
+
+//export WriteHeader
+func WriteHeader(writerID *C.char, header *C.char) C.pointerResponse {
+	w, err := getWriter(writerID)
+	if err != nil {
+		return pointerResponseError("could not get writer")
+	}
+	w.mu.Lock()
+	//w.writer.WithXHeaders()
+	//w.writer.WithinSession()
+	//w.writer.WithBearerToken()
+	o := object.New()
+	written := w.writer.WriteHeader(*o)
+	w.mu.Unlock()
+
+	return pointerResponseBoolean(written)
+}
+
+// Can be called multiple times (appended). If the chunk exceeds the maximal size, it is
+// automatically split (assumption in neofs-sdk-go: maximal chunk size of 3MB).
+//export WritePayloadChunk
+func WritePayloadChunk(writerID *C.char, chunk unsafe.Pointer, chunkSize C.int) C.pointerResponse {
+	bytes := C.GoBytes(chunk, chunkSize)
+	fmt.Println(bytes)
+
+	w, err := getWriter(writerID)
+	if err != nil {
+		return pointerResponseError("could not get writer")
+	}
+	w.mu.Lock()
+	//w.writer.WithXHeaders()
+	//w.writer.WithinSession()
+	//w.writer.WithBearerToken()
+	written := w.writer.WritePayloadChunk(bytes)
+	w.mu.Unlock()
+
+	return pointerResponseBoolean(written)
+}
+
+//export CloseWriter
+func CloseWriter(writerID *C.char) C.response {
+
+	w, err := getWriter(writerID)
+	if err != nil {
+		return responseError("could not get writer")
+	}
+	w.mu.Lock()
+	//w.writer.WithXHeaders()
+	//w.writer.WithinSession()
+	//w.writer.WithBearerToken()
+	res, err := w.writer.Close()
+	if err != nil {
+		return responseError(err.Error())
+	}
+	wMap.delete(writerID)
+	w.mu.Unlock()
+
+	if !apistatus.IsSuccessful(res.Status()) {
+		return resultStatusErrorResponse()
+	}
+
+	var oid oid.ID
+	read := res.ReadStoredObjectID(&oid)
+	if !read {
+		return responseError("could not read stored object id")
+	}
+	return response(reflect.TypeOf(oid), oid.String())
+}
+
+//region client
+
+var wMap *WriterMap
+
+type Writer struct {
+	mu     sync.RWMutex
+	writer *neofsclient.ObjectWriter
+}
+
+type WriterMap struct {
+	mu      sync.RWMutex
+	writers map[uuid.UUID]*Writer
+}
+
+func (wMap *WriterMap) put(id uuid.UUID, newWriter *neofsclient.ObjectWriter) {
+	wMap.mu.Lock()
+	wMap.writers[id] = &Writer{
+		mu:     sync.RWMutex{},
+		writer: newWriter,
+	}
+	wMap.mu.Unlock()
+}
+
+func (wMap *WriterMap) delete(writerID *C.char) bool {
+	id, err := uuid.Parse(C.GoString(writerID))
+	if err != nil {
+		return false
+	}
+	wMap.mu.Lock()
+	delete(wMap.writers, id)
+	wMap.mu.Unlock()
+	return true
+}
+
+func initWriterMap(id uuid.UUID, newWriter *neofsclient.ObjectWriter) {
+	wMap = &WriterMap{sync.RWMutex{}, map[uuid.UUID]*Writer{id: {sync.RWMutex{}, newWriter}}}
+}
+
+func getWriter(writerID *C.char) (*Writer, error) {
+	if wMap == nil {
+		return nil, fmt.Errorf("no writer present")
+	}
+	wID, err := uuid.Parse(C.GoString(writerID))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse provided writer id. id was " + C.GoString(writerID))
+	}
+	wMap.mu.Lock()
+	w := wMap.writers[wID]
+	if w == nil {
+		return nil, fmt.Errorf("no client present with id %v", C.GoString(writerID))
+	}
+	wMap.mu.Unlock()
+	return w, nil
+}
 
 ////export GetObjectInit
 //func GetObjectInit(clientID *C.char, v2ContainerID *C.char) *C.pointerResponse {
@@ -60,32 +231,6 @@ GetRangeHash
 //		return nil
 //	}
 //	return C.CString() // return pointer to object reader and
-//}
-
-////export PutObjectInit
-//func PutObjectInit(clientID *C.char, key *C.char) *C.pointerResponse {
-//	cli, err := getClient(key)
-//	if err != nil {
-//		return pointerResponseClientError()
-//	}
-//	cli.mu.RLock()
-//	ctx := context.Background()
-//
-//	var prmObjectPutInit neofsclient.PrmObjectPutInit
-//	objectWriter, err := cli.client.ObjectPutInit(ctx, prmObjectPutInit)
-//	cli.mu.RUnlock()
-//	if err != nil {
-//		panic(err)
-//	}
-//	objectWriter.UseKey()
-//	objectWriter.MarkLocal() // optional
-//	objectWriter.WithinSession()
-//	objectWriter.WithBearerToken()
-//	objectWriter.WritePayloadChunk()
-//	objectWriter.WriteHeader()
-//	objectWriter.Close() // in separate method or when all is read
-//
-//	return pointerResponse()
 //}
 
 ////export DeleteObject
